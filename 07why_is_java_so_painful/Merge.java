35e3d056f348ecab638e4cc8364d8b1072295606
George Zheng <georgezheng999@gmail.com>
2018-03-20 17:31:38 -0400

    yijes

---
+++ 07why_is_java_so_painful/Merge.java
@@ -0,0 +1,28 @@
+public class Merge{
+	
+	public static void main(String[] args){
+		
+		int[] arr = new int[] {999,999,999,4,1,0, 0,0,3,2,999,999,999};
+	
+		mergesort(arr);
+		System.out.println(Arrays.toString(arr));
+		
+	}
+
+	public static void mergesort(int[] data){
+
+	}
+
+	private static void msort(int[] data, int[] temp, int lo, int hi){
+
+	}
+
+	public static void merge(int[] data, int start1, int end1, int start2, int end2){
+		while (start1 < end1 || start1 < end1){
+		
+		}
+	}
+
+
+
+}
