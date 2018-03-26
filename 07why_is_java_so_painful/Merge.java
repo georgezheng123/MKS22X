370d633459517e95f4c88bdf0d2f95d2ce6caf3c
George Zheng <georgezheng999@gmail.com>
2018-03-25 23:11:25 -0400

    forgot

--- 07why_is_java_so_painful/Merge.java
+++ 07why_is_java_so_painful/Merge.java
@@ -1,18 +1,34 @@
 import java.util.*;
 
 public class Merge{
-	
-	public static void main(String[] args){
-		
-		int[] arr = new int[] {52, 12, 53, 1};
-		mergesort(arr);
 
-		// arr = new int[] {1, 5, 9, 2, 7, 19};
+	private static final int INCREASE = 0;
+	private static final int DECREASE = 1;
+	private static final int STANDARD = 2;
+	private static final int SMALL_RANGE = 3;
+	private static final int EMPTY = 4;
 
-		// merge(arr, 0, 5/2,5);
-		
-		System.out.println(Arrays.toString(arr));
-		
+	// public static void main(String[] args){
+	// 	int[] arr = makeArray(10000000, 2);
+	// 	int[] sorted = Arrays.copyOf(arr,arr.length);
+	// 	Arrays.sort(sorted);
+	// 	mergesort(arr);
+	// 	System.out.println(Arrays.toString(arr));
+	// 	System.out.println(Arrays.equals(arr, sorted));
+	// }
+
+	public static void insertionSort(int[] data, int lo, int hi){ 
+		int j = lo+1; 
+		while (j <= hi) {  
+			int current=data[j];  
+			int i = j-1;  
+			while (i>lo-1 && data[i]>current) {  
+				data[i+1] = data[i];  
+				i--;  
+			}  
+			data[i+1] = current;  
+			j++;
+		}  
 	}
 
 	public static void mergesort(int[] data){
@@ -22,9 +38,13 @@ public class Merge{
 	private static void msort(int[] data, int lo, int hi){
 		if (lo < hi){
 			int middle = (lo + hi) / 2;
-			msort(data, lo, middle);
-			msort(data, middle+1, hi);
-			merge(data, lo, middle, hi);
+			if (hi - lo > 20){
+				msort(data, lo, middle);
+				msort(data, middle+1, hi);
+				merge(data, lo, middle, hi);
+			}else{
+				insertionSort(data, lo, hi);
+			}
 		}
 	}
 	// {0, 3, 6, 1, 5, 7}
@@ -54,5 +74,76 @@ public class Merge{
 	}
 
 
+ //Sort testing code
+
+
+  private static String name(int i){
+    if(i==0)return "Increassing";
+    if(i==1)return "Decreassing";
+    if(i==2)return "Normal Random";
+    if(i==3)return "Random with Few Values";
+    if(i==4)return "size 0 array";
+    return "Error stat array";
+
+  }
+
+  private static int create(int min, int max){
+    return min + (int)(Math.random()*(max-min));
+  }
+
+  private static int[] makeArray(int size,int type){
+    int[]ans =new int[size];
+    if(type == STANDARD){
+      for(int i = 0; i < size; i++){
+        ans[i]= create(-1000000,1000000);
+      }
+    }
+    if(type == INCREASE){
+      int current = -5 * size;
+      for(int i = 0; i < size; i++){
+        ans[i]= create(current,current + 10);
+        current += 10;
+      }
+    }
+    if(type == DECREASE){
+      int current = 5 * size;
+      for(int i = 0; i < size; i++){
+        ans[i]= create(current,current + 10);
+        current -= 10;
+      }
+    }
+    if(type == SMALL_RANGE){
+      for(int i = 0; i < size; i++){
+        ans[i]= create(-5,5);
+      }
+    }
+    if(type == EMPTY){
+      ans = new int[0];
+    }
+    return ans;
+  }
+
+  public static void main(String[]args){
+    if(args.length < 2)return;
+    
+    int size =  Integer.parseInt(args[0]);
+    int type =   Integer.parseInt(args[1]);
+
+    int [] start = makeArray(size,type);
+    int [] result = Arrays.copyOf(start,start.length);
+    Arrays.sort(result);
+    
+    long startTime = System.currentTimeMillis();
+    
+    mergesort(start);
+     
+    long elapsedTime = System.currentTimeMillis() - startTime;
+ 
+    if(Arrays.equals(start,result)){
+      System.out.println("PASS Case "+name(type)+" array, size:"+size+" "+elapsedTime/1000.0+"sec ");
+    }else{
+      System.out.println("FAIL ! ERROR ! "+name(type)+" array, size:"+size+"  ERROR!");
+    }
+}
 
 }
